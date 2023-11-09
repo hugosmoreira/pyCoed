d99c1f7ddf20b5c7b98b171c91eba04fe6c87435
Hugo Moreira <hugo.n4@gmail.com>
2023-11-08 19:31:39 -0800

    update

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World!')
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+
+llm = OpenAI(
+    openai_api_key=api_key
+)
+
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers"
+})
+
+print(result["text"])
